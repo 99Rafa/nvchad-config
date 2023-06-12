276464a9a5180cda895fe8f7f2d9826f81c6dd83
Rafael Aguirre <rafael.aguirre.ro@gmail.com>
2023-06-12 00:02:15 -0600

    Telescope config

--- chadrc.lua
+++ chadrc.lua
@@ -1,13 +1,16 @@
 ---@type ChadrcConfig 
 local M = {}
 
-M.ui = {
-  theme = 'catppuccin',
-  transparency = true,
-}
-
 M.plugins = 'custom.plugins'
 
 M.mappings = require "custom.mappings"
+
+M.ui = {
+  theme = 'catppuccin',
+  transparency = true,
+  statusline = {
+    theme = "minimal"
+  }
+}
 
 return M
--- mappings.lua
+++ mappings.lua
@@ -9,10 +9,19 @@ M.disabled = {
     -- tabufline
     ["<tab>"] = "",
     ["<S-tab>"] = "",
-    ["<leader>q"] = "",
+    ["<leader>x"] = "",
 
     -- nvimtree
     ["<leader>e"] = "",
+
+    -- telescope
+    ["<leader>ff"] = "",
+    ["<leader>fa"] = "",
+    ["<leader>fw"] = "",
+    ["<leader>fb"] = "",
+    ["<leader>fh"] = "",
+    ["<leader>fo"] = "",
+    ["<leader>fz"] = "",
   }
 }
 
@@ -98,6 +107,18 @@ M.tabufline = {
       "Close buffer",
     },
   },
+}
+
+M.telescope = {
+  n = {
+    ["<leader>sf"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+    ["<leader>sa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
+    ["<leader>sg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+    ["<leader>sb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
+    ["<leader>sh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
+    ["<leader>so"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
+    ["<leader>sz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+  }
 }
 
 return M
